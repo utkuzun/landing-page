fe901435c4c4eeb28ca2495ff0c10f1d1d649b1d
utkuzun <utkuzun50@gmail.com>
2023-04-17 17:08:13 +0300

    apply class added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <section id='hero'>
-      <div className='flex flex-col-reverse md:flex-row container px-6 items-center mx-auto mt-10 space-y-0  md:space-y-0'>
+      <div className='section-double'>
         <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
           <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
             Bring everyone together to build better products.
